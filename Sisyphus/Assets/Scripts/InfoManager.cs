36cb1a481efd5ecf8f2859c60f1fa4888854e52c
unknown <dmactte1995@gmail.com>
2020-11-16 18:43:25 +0000

    2020/11/16 v1

---
+++ Sisyphus/Assets/Scripts/InfoManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoManager : MonoBehaviour
+{
+    public const int Level1 = 1;
+    
+    public Texture backGround;
+    
+    private int gameStage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gameStage)
+        {
+            case 1:
+                CreateLevel1();
+                break;
+        }
+    }
+
+    void CreateLevel1()
+    {
+        //GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),backGround);
+    }
+}
---
+++ Sisyphus/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float movespeed = 1;
+    private Rigidbody2D _rigidbody;
+    private int force;
+    private int maxForce;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        force = 2;
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rigidbody.AddForce(new Vector3(-1*force, 0, 0) * movespeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.AddForce(new Vector3(force, 0, 0) * movespeed);
+        }
+    }
+}
