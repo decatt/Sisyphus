1d5d7ef7e850889dcac4d2c18493da5e24e78782
unknown <dmactte1995@gmail.com>
2020-11-17 16:28:56 +0000

    v2

--- Sisyphus/Assets/Scripts/MovePlayer.cs
+++ Sisyphus/Assets/Scripts/MovePlayer.cs
@@ -6,8 +6,9 @@ public class MovePlayer : MonoBehaviour
 {
     public float movespeed = 1;
     private Rigidbody2D _rigidbody;
-    private int force;
-    private int maxForce;
+    private int force=200;
+    private int maxForce=500;
+    private int minForce=200;
     
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,7 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        force = 2;
+
         if (Input.GetKey(KeyCode.A))
         {
             _rigidbody.AddForce(new Vector3(-1*force, 0, 0) * movespeed);
@@ -26,6 +27,16 @@ public class MovePlayer : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             _rigidbody.AddForce(new Vector3(force, 0, 0) * movespeed);
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(force<maxForce)
+            {
+                force+=10;
+            }
+        }
+        else if(force>minForce){
+            force-=10;
         }
     }
 }
